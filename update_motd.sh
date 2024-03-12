781be96291cbba921d68feae99af6cf8c6fc995a
Cam Ratchford <camratchford@gmail.com>
2024-03-12 21:18:48 +0000

    Added 2 different bash helper scripts

--- update_motd.sh
+++ update_motd.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root" 1>&2
-  exit 100
-fi
+check_root
+function update_motd() {
+    UPDATE_MOTD="/etc/update-motd.d"
+    rm -rf "$UPDATE_MOTD/{*,.*}"
+    # Expand tar.gz
+    tar -xzvf update-motd.tar.gz -C /
+}
 
-UPDATE_MOTD="/etc/update-motd.d"
+ynprompt -p "This script will delete the current contents of /etc/update-motd.d/. Continue?" -c "update_motd"
 
-# confirm overwrite
-ansi --bold --white "This script will delete the current contents of /etc/update-motd.d/"
-echo ""
-ansi --bold --white "  Continue?"
-ansi --white "y|n"
-read PROMPT
-if [[ $PROMPT == 'n' || $PROMPT == 'N' ]]; then
-  exit
-fi
-
-# Delete existing
-rm -rf "$UPDATE_MOTD/{*,.*}"
-
-# Expand tar.gz
-tar -xzvf update-motd.tar.gz -C /
