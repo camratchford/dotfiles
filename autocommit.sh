ca0ef2b09daa8e29cbd8155df767b9d49493278e
Cam Ratchford <camratchford@gmail.com>
2024-11-14 13:07:19 -0700

    Autocommit Thu Nov 14 13:07:19 MST 2024

--- autocommit.sh
+++ autocommit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 THIS_DIR="$(dirname $(realpath $0))"
-
+DEFAULT_BRANCH="$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')"
 cd $THIS_DIR
 git add .
 git commit -m "Autocommit $(date)"
-git push -f origin main
+git push -f origin "$DEFAULT_BRANCH"
 
 
 
