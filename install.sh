6054231417116a89a0e9c202487bbcfecc684879
Cam Ratchford <camratchford@gmail.com>
2026-07-26 08:34:46 -0600

    Remove uninstall script gen from install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,20 @@
 #!/bin/bash -i
 
 THIS_DIR="$(dirname "$(realpath "$0")")"
-UNINSTALL_SCRIPT="$THIS_DIR/uninstall.sh"
 
-echo '#!/bin/bash' > "$UNINSTALL_SCRIPT"
-chmod 755 "$UNINSTALL_SCRIPT"
 BACKUP_DIR="$THIS_DIR/backup"
-
-if [ -d "$BACKUP_DIR" ] && ! [ "$(ls -A "$BACKUP_DIR")" ]; then
-  now="$(date +%Y_%m_%d+%H-%M-%S)"
-  archive_name="dotfiles-backup-$now.tar.gz"
-  cd "$BACKUP_DIR" && tar cz ./ > "../$archive_name"
-fi
 mkdir -p "$BACKUP_DIR"
 
 function link-dir {
   local src="$1"
   local target="$2"
 
-  if [ -d "$target/$(basename "$src")" ] && ! [ -L "$target/$(basename "$src")" ] ;then
+  if [ -d "$target/$(basename "$src")" ] && ! [ -L "$target/$(basename "$src")" ]; then
     mv "$target/$(basename "$src")" "$BACKUP_DIR/$(basename "$src")"
   fi
 
   mkdir -p "$target"
-
   ln -s "$src" "$target/" 2> /dev/null
-  echo "rm -f $target/$(basename "$src")" >> "$UNINSTALL_SCRIPT"
 }
 
 # Deal with sym-linking files
@@ -38,7 +27,6 @@ for file in $DOTFILES; do
     rm "$home_name"
   fi
   ln -s "$file" "$home_name"
-  echo "rm -f $home_name" >> "$UNINSTALL_SCRIPT"
 done
 
 # Specify the exact location of the dotdirs
@@ -74,11 +62,6 @@ for period in hourly daily weekly monthly; do
   if ! [[ ${CRONTAB_CONTENTS[*]} =~ $CRON_JOB ]]; then
     CRONTAB_CONTENTS+=("$CRON_JOB")
   fi
-
-  # Allow undoing
-  CLEAN_CRON_JOB=$(printf %s "$CRON_JOB" | sed 's:/:\\/:g')
-  sed_cmd="/${CLEAN_CRON_JOB}/d"
-  echo "crontab -l 2> /dev/null | sed -e '$sed_cmd' | crontab -" >> "$UNINSTALL_SCRIPT"
 done
 
 CRONTAB_ENV=(
@@ -99,18 +82,15 @@ if which konsole &> /dev/null; then
   kwriteconfig5 --file konsolerc --group "Desktop Entry" --key DefaultProfile "Custom.profile"
 fi
 
-# Install my Vim plugin manager
+# Install Pathogen, a Vim plugin manager
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 if ! [ -f ~/.vim/autoload/pathogen.vim ]; then
   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-  echo "rm ~/.vim/autoload/pathogen.vim" >> "$UNINSTALL_SCRIPT"
 fi
 
 # Add Vim plugins
 git submodule init
 git submodule update --init --recursive --depth 1
-# shellcheck disable=SC2016
-git submodule foreach 'echo rm -rf $sm_path >> $toplevel/uninstall.sh > /dev/null' > /dev/null
 
 # Install Vim plugin docs
 DOCS_DIRS="$(find ~/.vim/bundle -path "*/doc")"
